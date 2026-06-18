ac4b9f9e9cbb942c665425f1ea26ab2191f2293f
Atttednde <jeongs9663@gmail.com>
2026-06-18 14:14:25 +0900

    민감 폴더 제외 및 .gitignore 정리

--- korea-webtoon-api-main/src/constants/routes.ts
+++
@@ -1,8 +0,0 @@
-export enum ROUTES {
-  SWAGGER = '/api-docs',
-  UPDATE_NAVER = '/update/naver',
-  UPDATE_KAKAO = '/update/kakao',
-  UPDATE_KAKAO_PAGE = '/update/kakao-page',
-  HEALTH_CHECK = '/health-check',
-  GET_WEBTOONS = '/webtoons',
-}
